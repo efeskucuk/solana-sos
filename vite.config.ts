7b11d0b1cd2500380115dfb5e52506958de40d73
Efe Selim Kucuk <efeskucuk@gmail.com>
2024-10-20 23:37:29 +0300

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/solana-sos/",
   plugins: [react(),nodePolyfills()],
   envDir: "env"
 })
